6eb32a2f7ca74720cfcbc5cacd83f6c0cc130b73
Keith Bennett <keithrbennett@gmail.com>
2012-12-15 02:46:19 -0500

    Added Problem 8.

--- spec/euler_spec.rb
+++ spec/euler_spec.rb
@@ -21,11 +21,21 @@ describe Euler do
   it "should calculate euler3 correctly" do
     pending
     answer = subject.euler
+    STDERR.puts "problem 3 answer is #{answer}"
     subject.euler3.should == 1  # replace with correct value
+  end
+
+  it "should calculate euler8 correctly" do
+    # Found max: num = 40824, offset = 364, digits = [9, 9, 8, 7, 9]
+    subject.euler8.should == 40824
   end
 
   it "should calculate euler13 correctly" do
     subject.euler13.should == '5537376230'
   end
 
+  it "should create a digit array correctly" do
+    expected = [7, 5, 0, 2, 8, 3, 4]
+    subject.digit_array(7502834).should == expected
+  end
 end
