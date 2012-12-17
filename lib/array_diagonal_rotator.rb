c065af0f779ecadbc077689ed45f1e9a475194d6
Keith Bennett <keithrbennett@gmail.com>
2012-12-17 17:31:21 -0500

    Working on rotate_left_45, not working, but want to commit and work on rotate_180.

--- lib/array_diagonal_rotator.rb
+++ lib/array_diagonal_rotator.rb
@@ -1,18 +1,43 @@
 class ArrayDiagonalRotator
 
-  def create_output_array(input_array)
-    height = input_array.size
-    width = input_array.first.size
-    num_output_inner_arrays = height + width - 1
-    Array.new(num_output_inner_arrays)
+  attr_reader :input_array, :num_output_inner_arrays
+
+  def initialize(input_array)
+    @input_array = input_array
+    @y_size = input_array.size
+    @x_size = input_array.first.size
+    @y_max = @y_size - 1
+    @x_max = @x_size - 1
+    @num_output_inner_arrays = @y_size + @x_size - 1
   end
 
-  def rotate(input_array)
-    ia = input_array
-    oa = []  # output array
-    oa << [ia[-1][0]]
-    oa << [ia[-2][0], ia[-1][1]]
-    oa << [ia[0][-1]]
-    oa
+  def create_output_array
+    Array.new(@num_output_inner_arrays)
   end
+
+  # Input array must be a 2D array whose inner arrays
+  # sizes are all equal
+  def rotate_45_left
+    outer_output_array = []  # output array
+    start_y, start_x = 0, @x_max
+    outer_output_array << [input_array[start_y][start_x]]
+    (0..@num_output_inner_arrays).each do |input_outer_array_index|
+      #start y =
+      inner_array = []
+
+
+    end
+    last_outer = ia.size - 1
+    last_inner = ia.first.size - 1
+    (last_outer.downto(0)).each do |outer_index|
+      inner_array = []
+      0.upto(last_inner) do |inner_index|
+        inner_array << ia[inner_index][outer_index + inner_index]
+      end
+      outer_output_array << inner_array
+    end
+    outer_array
+  end
+
+
 end
--- spec/array_diagonal_rotator_spec.rb
+++ spec/array_diagonal_rotator_spec.rb
@@ -4,26 +4,28 @@ require "array_diagonal_rotator"
 
 describe ArrayDiagonalRotator do
 
+  subject { ArrayDiagonalRotator.new([[0, 1], [2, 3]]) }
+
   # The number of arrays required will be (height + width - 1) of original array.
   it "should create an array of x + y - 1 arrays" do
     input_array = Array.new(4) { Array.new(3) }
-    output_array = subject.send(:create_output_array, input_array)
-    output_array.length.should == 6
+    output_array = subject.send(:create_output_array)
+    output_array.length.should == 3
   end
 
-  it "'s first array should have 1 element whose content is x_0, y_max " do
-    a = [[0, 1], [2, 3]]
-    subject.rotate(a).first.should == [2]
+  it "'s first array should be [1] " do
+    pending
+    subject.rotate_45_left.first.should == [1]
   end
 
   it "'s second array should be [0, 3]" do
-    a = [[0, 1], [2, 3]]
-    subject.rotate(a)[1].should == [0, 3]
+    pending
+    subject.rotate_45_left[1].should == [2, 1]
   end
 
   it "'s third array should be [1]" do
-    a = [[0, 1], [2, 3]]
-    subject.rotate(a)[2].should == [1]
+    pending
+    subject.rotate_45_left[2].should == [3]
   end
 
 end
