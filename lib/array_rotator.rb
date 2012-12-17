3851e4fc7fe66af5bbeb3f844b47e0e813a30059
Keith Bennett <keithrbennett@gmail.com>
2012-12-17 18:59:39 -0500

    Made some instance variables functions instead, to accomodate mutable data_array.

--- lib/array_rotator.rb
+++ lib/array_rotator.rb
@@ -4,15 +4,30 @@ class ArrayRotator
 
   def initialize(data_2d_array)
     @data_array = data_2d_array
-    @y_size = data_array.size
-    @x_size = data_array.first.size
-    @y_max = @y_size - 1
-    @x_max = @x_size - 1
-    @num_output_inner_arrays = @y_size + @x_size - 1
+  end
+
+  def y_size
+    data_array.size
+  end
+
+  def x_size
+    data_array.first.size
+  end
+
+  def y_max
+    y_size - 1
+  end
+
+  def x_max
+    x_size - 1
+  end
+
+  def num_output_inner_arrays
+    y_size + x_size - 1
   end
 
   def create_output_array
-    Array.new(@num_output_inner_arrays)
+    Array.new(num_output_inner_arrays)
   end
 
 
@@ -23,15 +38,15 @@ class ArrayRotator
 
   def upper_right_to_lower_left_coords
     points = []
-    @x_max.downto(0) { |x| points << [0, x] }
-    1.upto(@y_max)   { |y| points << [y, 0]}
+    x_max.downto(0) { |x| points << [0, x] }
+    1.upto(y_max)   { |y| points << [y, 0]}
     points
   end
 
   def inner_line_coords(start_xy)
     y, x = start_xy
     coords = []
-    while y <= @y_max
+    while y <= y_max
       coords << [y, x]
       y += 1
       x += 1
@@ -42,24 +57,11 @@ class ArrayRotator
   # Input array must be a 2D array whose inner arrays
   # sizes are all equal
   def rotate_45_left
-    input_array = @data_array
     new_array = []  # output array
     upper_right_to_lower_left_coords.each do |start_yx|
       inner_array = []
-      inner_array << value_at(start_yx)
-        new_array
-
-      #start y =
-      inner_array = []
-
-
-    end
-    last_outer = ia.size - 1
-    last_inner = ia.first.size - 1
-    (last_outer.downto(0)).each do |outer_index|
-      inner_array = []
-      0.upto(last_inner) do |inner_index|
-        inner_array << ia[inner_index][outer_index + inner_index]
+      inner_line_coords.each do |yx|
+        inner_array << value_at(start_yx)
       end
       new_array << inner_array
     end
